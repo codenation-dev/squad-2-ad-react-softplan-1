2c61b88a2db8cff07baeb78104c81956325d9f2e
fernandojvlima <fernandojvlima@gmail.com>
2019-12-04 16:57:57 -0300

    Uploading Login and Register with Validation

--- frontend/src/components/Events.js
+++ frontend/src/components/Events.js
@@ -1,47 +1,48 @@
-// import React from "react";
-// import { Filter } from "./Filter";
-// import { List } from "./List";
-// import { Container } from 'react-bootstrap';
-// import { getEventos, getEventosByLevel } from "../Api";
+import React from "react";
+import { Filter } from "./Filter";
+import { List } from "./List";
+import { Container } from 'react-bootstrap';
+import { getEventos, getEventosByLevel } from "../Api";
 
-// class Events extends React.Component {
+class Events extends React.Component {
 
-//     state = {
-//       eventos: [],
-//       loading: true,
-//       error: null,
-//     };
+  state = {
+    eventos: [],
+    loading: true,
+    error: null,
+  };
 
-//     getEventos = async () => {
-//       try {
-//         const eventos = await getEventos();
-//         this.setState({ eventos, loading: false });
-//       } catch (error) {
-//         this.setState({ error, loading: false });
-//       }
-//     };
+  getEventos = async () => {
+    try {
+      const eventos = await getEventos();
+      this.setState({ eventos, loading: false });
+    } catch (error) {
+      this.setState({ error, loading: false });
+    }
+  };
 
 
-//     getEventosByLevel = async (event) => {
-//       try {
-//         const eventos = await getEventosByLevel(event);
-//         this.setState({ eventos, loading: false });
-//       } catch (error) {
-//         this.setState({ error, loading: false });
-//       }
-//     };
+  getEventosByLevel = async (event) => {
+    try {
+      const eventos = await getEventosByLevel(event);
+      this.setState({ eventos, loading: false });
+    } catch (error) {
+      this.setState({ error, loading: false });
+    }
+  };
 
-//     componentDidMount() {
-//       this.getEventos();
-//     }
+  componentDidMount() {
+    this.getEventos();
+  }
 
-//     render(){
-//     return (
-//       <Container fluid="true">
-//         <Filter getEventosByLevel={this.getEventosByLevel} />
-//         <List eventos={this.state.eventos}/>
-//       </Container>
-//     )}
-// }
+  render() {
+    return (
+      <Container fluid="true">
+        <Filter getEventosByLevel={this.getEventosByLevel} />
+        <List eventos={this.state.eventos} />
+      </Container>
+    )
+  }
+}
 
-// export default Events
+export default Events
