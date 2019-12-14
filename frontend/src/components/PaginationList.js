947a992570d423bff9e433e3a40ad1cfafac93d2
Eduardo Kraus Nunes <ekrausnunes@Eduardos-MacBook-Pro.local>
2019-12-14 13:19:50 -0300

    Retirando warnings, ajuste footer.

--- frontend/src/components/PaginationList.js
+++ frontend/src/components/PaginationList.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Pagination, Col } from "react-bootstrap";
 
 function PaginationList({ eventos, pagination, setPagination }) {
